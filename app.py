71af930a63a03f293628fad7b6a4cfc8f9f7384b
arnau <arnaudv2003@gmail.com>
2025-03-24 20:30:52 +0100

    Working on good api

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from flask_cors import CORS
 
-load_dotenv()  # Charge les variables d'environnement
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -52,54 +52,6 @@ def send_email():
                 "Subject": subject,
                 "TextPart": text,
                 "HTMLPart": html
-            }
-        ]
-    }
-
-    try:
-        response = requests.post(mailjet_url, json=payload, auth=(MAILJET_API_KEY, MAILJET_API_SECRET), headers=headers)
-        response_data = response.json()
-
-        if response.status_code == 200:
-            return jsonify({"success": True, "data": response_data}), 200
-        else:
-            return jsonify({"success": False, "error": response_data}), response.status_code
-    except Exception as e:
-        return jsonify({"success": False, "error": str(e)}), 500
-
-
-@app.route('/send-email/notification', methods=['POST'])
-def send_email_notification():
-    data = request.get_json()
-
-    required_fields = ["name", "surname", "email", "services", "message", "language"]
-    missing_fields = [field for field in required_fields if field not in data]
-
-    if missing_fields:
-        return jsonify({"success": False, "error": f"Champs manquants: {', '.join(missing_fields)}"}), 400
-
-    name = data["name"]
-    surname = data["surname"]
-    email = data["email"]
-    services = data["services"]
-    message = data["message"]
-    language = data["language"]
-
-    html_content = get_html(language, 'notification')
-    if html_content is None:
-        return jsonify({"success": False, "error": "Template HTML non trouvé pour la langue spécifiée"}), 404
-
-    html_content = html_content.format(name=name, surname=surname, email=email, services=", ".join(services), message=message)
-
-    mailjet_url = "https://api.mailjet.com/v3.1/send"
-    headers = {"Content-Type": "application/json"}
-    payload = {
-        "Messages": [
-            {
-                "From": {"Email": SENDER_EMAIL, "Name": "4CORES CONTACT"},
-                "To": [{"Email": email, "Name": f"{name} {surname}"}],
-                "Subject": "Notification de contact 4CORES",
-                "HTMLPart": html_content
             }
         ]
     }
